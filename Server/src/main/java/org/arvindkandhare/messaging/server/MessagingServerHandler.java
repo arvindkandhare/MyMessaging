bfd4e65ab1b1190846bd659f519200b10d34671f
arvindkandhare <arvind.kandhare@emc.com>
2016-09-25 16:01:40 -0700

    Adding initial commit

---
+++ Server/src/main/java/org/arvindkandhare/messaging/server/MessagingServerHandler.java
@@ -0,0 +1,28 @@
+package org.arvindkandhare.messaging.server;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by kandha on 9/25/16.
+ */
+public class MessagingServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
